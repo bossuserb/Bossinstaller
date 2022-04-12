a72f58a0486878d6138df6e8e1f4b4790cf84f80
bossuserb <103330740+bossuserb@users.noreply.github.com>
2022-04-12 04:49:48 +0400

    Update __main__.py

--- boss_installer/__main__.py
+++ boss_installer/__main__.py
@@ -3,7 +3,7 @@ from time import time
 import random
 import requests
 from git import Repo
-from up_qurulum import *
+from bossinstaller import *
 from .astring import main
 import os
 from telethon import TelegramClient, functions
